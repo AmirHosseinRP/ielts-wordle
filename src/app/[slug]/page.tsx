188b8ed8a8040d7fb2a0bad09753fca74f01ac6b
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2023-10-18 18:04:04 +0330

    feat: the word itself added to word page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,14 +8,22 @@ export default function WordPage({ params }: { params: { slug: string } }) {
     return slugWord.word === params.slug;
   });
   return (
-    <section className="p-10 flex flex-col items-center justify-between w-full h-[90vh]">
+    <section className="p-10 flex flex-col items-center justify-between w-full h-[90vh] text-center">
       {word && (
-        <div className="text-center">
-          <p className="text-3xl mb-2">Meaning: </p>
-          <p>{word.meaning}</p>
-          <p className="text-3xl mb-2 mt-24">Example:</p>
-          <p>{word.example}</p>
-        </div>
+        <>
+          <div className="gap-2">
+            <p className="text-3xl mb-2">Word: </p>
+            <p>{word.word}</p>
+          </div>
+          <div className="gap-2">
+            <p className="text-3xl mb-2">Meaning: </p>
+            <p>{word.meaning}</p>
+          </div>
+          <div className="gap-2">
+            <p className="text-3xl mb-2">Example:</p>
+            <p>{word.example}</p>
+          </div>
+        </>
       )}
       <Link href="/" className="w-full mb-10 max-w-xs">
         <Button variant="contained" color="primary" className="bg-blue-700 w-full">
